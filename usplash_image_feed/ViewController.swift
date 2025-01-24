48451555e99fe049825fa7d8ce4022d87523b862
Vlad Mironov <73903449+vv-m@users.noreply.github.com>
2025-01-24 12:38:11 +0300

    Initial Commit

---
+++ usplash_image_feed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  usplash_image_feed
+//
+//  Created by Влад Миронов on 24.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
