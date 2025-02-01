40bb4f81c28d549455afd8a8c19bb53179baaf5f
Vlad Mironov <vlad372@yandex.ru>
2025-02-01 22:23:52 +0300

    feat: отображение таблицы со списком фрутков

--- usplash_image_feed/ViewController.swift
+++ usplash_image_feed/ViewController.swift
@@ -1,12 +1,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let words = ["Apple", "Pear", "Watermelon", "Carrot", "Pickle", "Potato", "Tomato"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
 
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return words.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell                                                   // 1
+        
+        if let reusedCell = tableView.dequeueReusableCell(withIdentifier: "cell") { // 2
+            cell = reusedCell
+        } else {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "cell")        // 3
+        }
+        
+        cell.textLabel?.text = words[indexPath.row]                                 // 4
+        
+        return cell
+    }
 }
 
