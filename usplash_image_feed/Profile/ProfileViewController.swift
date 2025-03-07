faa29f73ad4e1a574c1b4d82a5f5d297e8492b0b
Vlad Mironov <vlad372@yandex.ru>
2025-03-07 22:23:11 +0300

    fit: Сдача спринта - попытка 1

--- usplash_image_feed/Profile/ProfileViewController.swift
+++ usplash_image_feed/Profile/ProfileViewController.swift
@@ -1,15 +1,81 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-    @IBOutlet weak var nameUserLabel: UILabel!
-    @IBOutlet weak var loginUserLabel: UILabel!
-    @IBOutlet weak var textUserLabel: UILabel!
-    
-    @IBOutlet weak var exitButton: UIButton!
+    private var photo: UIImageView!
+    private var nameUserLabel: UILabel!
+    private var loginUserLabel: UILabel!
+    private var textUserLabel: UILabel!
+    private var exitButton: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        viewPhoto()
+        viewName()
+        viewLoginUserLabel()
+        viewTextUserLabel()
+        viewExitButton()
     }
+    
+    private func viewPhoto() {
+        photo = UIImageView(image: UIImage(named: "Photo"))
+        view.addSubview(photo)
+        photo.translatesAutoresizingMaskIntoConstraints = false
+        
+        photo.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        photo.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32).isActive = true
+        photo.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        photo.heightAnchor.constraint(equalToConstant: 70).isActive = true
+    }
+    
+    private func viewName() {
+        nameUserLabel = UILabel()
+        view.addSubview(nameUserLabel)
+        nameUserLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameUserLabel.text = "Екатерина Новикова"
+        nameUserLabel.textColor = .white
+        nameUserLabel.font = UIFont.systemFont(ofSize: 23, weight: .bold)
+        
+        nameUserLabel.leadingAnchor.constraint(equalTo: photo.leadingAnchor).isActive = true
+        nameUserLabel.topAnchor.constraint(equalTo: photo.bottomAnchor, constant: 8).isActive = true
+    }
+    
+    private func viewLoginUserLabel() {
+        loginUserLabel = UILabel()
+        view.addSubview(loginUserLabel)
+        loginUserLabel.translatesAutoresizingMaskIntoConstraints = false
+        loginUserLabel.text = "@ekaterina_nov"
+        loginUserLabel.textColor = UIColor(red: 174/255, green: 175/255, blue: 180/255, alpha: 1.0)
+        loginUserLabel.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        
+        loginUserLabel.leadingAnchor.constraint(equalTo: photo.leadingAnchor).isActive = true
+        loginUserLabel.topAnchor.constraint(equalTo: nameUserLabel.bottomAnchor, constant: 8).isActive = true
+    }
+    
+    private func viewTextUserLabel() {
+        textUserLabel = UILabel()
+        view.addSubview(textUserLabel)
+        textUserLabel.translatesAutoresizingMaskIntoConstraints = false
+        textUserLabel.text = "Hello, world!"
+        textUserLabel.textColor = .white
+        textUserLabel.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+
+        textUserLabel.leadingAnchor.constraint(equalTo: photo.leadingAnchor).isActive = true
+        textUserLabel.topAnchor.constraint(equalTo: loginUserLabel.bottomAnchor, constant: 8).isActive = true
+    }
+    
+    private func viewExitButton() {
+        exitButton = UIButton()
+        view.addSubview(exitButton)
+        exitButton.translatesAutoresizingMaskIntoConstraints = false
+        exitButton.setImage(UIImage(named: "Exit"), for: .normal)
+        
+        exitButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -24).isActive = true
+        exitButton.centerYAnchor.constraint(equalTo: photo.centerYAnchor).isActive = true
+        exitButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        exitButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    
 }
--- usplash_image_feed/SingleImageViewController.swift
+++ usplash_image_feed/SingleImageViewController.swift
@@ -1,25 +1,99 @@
 import UIKit
 
-class SingleImageViewController: UIViewController {
+final class SingleImageViewController: UIViewController {
     var image: UIImage? {
         didSet {
-            guard isViewLoaded else {
+            guard isViewLoaded, let image else {
                 return
             }
             imageView.image = image
+            imageView.frame.size = image.size
+            rescaleAndCenterImageInScrollView(image: image)
         }
     }
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet private var imageView: UIImageView!
     
     @IBAction func didTapBackButton(_ sender: UIButton) {
         print("Pressed button")
         dismiss(animated: true, completion: nil)
     }
     
-    @IBOutlet private var imageView: UIImageView!
+    @IBAction func didTapShareButton(_ sender: Any) {
+        print("pressed the share button")
+        guard let image else {
+            return
+        }
+        let activityViewController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        
+        present(activityViewController, animated: true, completion: nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Конфигурируем scrollView
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+        
+        // Устанавливаем изображение
+        guard let image else { return }
         imageView.image = image
+        imageView.frame.size = image.size
+        rescaleAndCenterImageInScrollView(image: image)
+    }
+    
+    /// Масштабирует и центрирует изображение в ScrollView
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        // Получаем минимальный и максимальный масштаб из ScrollView
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        
+        // Обновляем макет представления, чтобы получить актуальные размеры
+        view.layoutIfNeeded()
+        
+        // Получаем видимую область ScrollView
+        let visibleRectSize = scrollView.bounds.size
+        print("visibleRectSize.width \(visibleRectSize.width)")
+        print("visibleRectSize.height \(visibleRectSize.height)")
+        
+        // Получаем размер изображения
+        let imageSize = image.size
+        print("imageSize.width \(imageSize.width)")
+        print("visibleRectSize.height \(imageSize.height)")
+        
+        // Вычисляем коэффициенты масштабирования по ширине и высоте
+        // Это нужно, чтобы изображение поместилось в видимую область
+        let hScale = (visibleRectSize.width / imageSize.width)
+        let vScale = visibleRectSize.height / imageSize.height
+        print("hScale \(hScale)")
+        print("vScale \(vScale)")
+        
+        // Выбираем наименьший масштаб, чтобы изображение целиком поместилось на экране,
+        // но при этом не выходило за пределы допустимых значений масштаба
+        let scale = min(maxZoomScale, max(minZoomScale, min(hScale, vScale)))
+        print("scale \(scale)")
+        
+        // Устанавливаем вычисленный масштаб
+        scrollView.setZoomScale(scale, animated: true)
+        
+        // Обновляем макет ScrollView, чтобы применить новый масштаб
+        scrollView.layoutIfNeeded()
+        
+        // Получаем новый размер контента после масштабирования
+        let newContentSize = scrollView.contentSize
+        
+        // Вычисляем смещение для центрирования изображения
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        // Устанавливаем смещение контента для центрирования изображения
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: true)
+    }
+}
+
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
     }
 }
