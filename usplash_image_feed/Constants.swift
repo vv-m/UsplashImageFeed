f42365628258565602fbf7e9bb8991d85500db37
Vlad Mironov <vlad372@yandex.ru>
2025-03-19 11:35:07 +0300

    added constants

---
+++ usplash_image_feed/Constants.swift
@@ -0,0 +1,5 @@
+enum Constants: String {
+    case accessKey = "AiraqGi2HJZeNArzn2lLwlGauw3Y-x1wzdaiUuA8D0A"
+    case secretKey = "lnxI3ec601R_YWBwBqmiAIyGz4lEn2Vuz40nfKuE0g4"
+    case redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+}
